567570a9d788daa21ff8d319740306781fa9628e
Julien-76 <julien.pothen@gmail.com>
2020-11-05 13:56:11 +0100

    travail sur la date

--- GestionStage/src/be/technifutur/java2020/gestionStage/stage/Stage.java
+++ GestionStage/src/be/technifutur/java2020/gestionStage/stage/Stage.java
@@ -1,11 +1,19 @@
 package be.technifutur.java2020.gestionStage.stage;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class Stage {
     private String key;
     private LocalDateTime dateDebut;
     private LocalDateTime dateFin;
 
+    public static void main(String[] args) {
+        LocalDateTime baba = LocalDateTime.of(2020,05,12,16,00);
+        System.out.println(baba);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        String nouvelleDate = baba.format(formatter);
+        System.out.println(nouvelleDate);
+    }
 
     public Stage (String nom, LocalDateTime debut, LocalDateTime fin) {
         this.key = nom;
